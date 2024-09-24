53a371c30e3530ab10ffc7a3d7daec6044666661
utkarsh232005 <utkarshpatrikar@gmail.com>
2024-09-24 18:52:41 +0530

    target blank

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,13 +16,13 @@ const Footer = () => {
 					<a href="mailto:utkarshpatrikar@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
 						<FiMail className='text-xl' />
 					</a>
-					<a href="?" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+					<a href="?" target='_blank' rel="noreferrer"  className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
 						<FaTwitter className='text-xl' />
 					</a>
-					<a href="www.linkedin.com/in/utkarsh-patrikar-649869267" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+					<a href="www.linkedin.com/in/utkarsh-patrikar-649869267" target='_blank' rel="noreferrer"  className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
 						<AiFillLinkedin className='text-xl' />
 					</a>
-					<a href="https://github.com/utkarsh232005" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
+					<a href="https://github.com/utkarsh232005" target='_blank' rel="noreferrer"  className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
 						<FaGithub className='text-xl' />
 					</a>
 				</div>
